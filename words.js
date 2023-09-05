6b2fcfbbb9764a4aa71516c42088251972019bfe
Malapasta <113771327+rqmaot@users.noreply.github.com>
2023-09-05 16:16:34 -0400

    Create words.js

---
+++ words.js
@@ -0,0 +1,11 @@
+ const wordList = [
+        { word: '1', pronunciation: 'Privet', translation: 'Hello' },
+        { word: '2', pronunciation: 'Dom', translation: 'House' },
+        { word: '3', pronunciation: 'Mama', translation: 'Mom' },
+        { word: '4', pronunciation: 'Papa', translation: 'Dad' },
+        { word: '5', pronunciation: 'Privet', translation: 'Hello' },
+        { word: '6', pronunciation: 'Dom', translation: 'House' },
+        { word: '7', pronunciation: 'Mama', translation: 'Mom' },
+        { word: '8', pronunciation: 'Papa', translation: 'Dad' },
+        // Add more words here as needed
+    ];
